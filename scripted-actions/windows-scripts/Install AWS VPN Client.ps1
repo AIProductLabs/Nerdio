4e8a00a699632b3633cb5c19016cc8c9b93c530f
Jason Sears <jasonrsears@gmail.com>
2024-07-16 20:55:30 -0500

    Install AWS VPN Client script added

---
+++ scripted-actions/windows-scripts/Install AWS VPN Client.ps1
@@ -0,0 +1,54 @@
+#description: Downloads and installs AWS VPN Client on the session hosts
+#Written by Jason Sears
+#No warranties given for this script
+#execution mode: IndividualWithRestart
+#tags: Nerdio, Apps install, AWS VPN Client
+<# 
+Notes:
+This script installs AWS VPN Client on AVD Session hosts.
+
+#>
+
+$AwsVpnClientUrl = "https://d20adtppz83p9s.cloudfront.net/WPF/latest/AWS_VPN_Client.msi"
+
+# Start powershell logging
+$SaveVerbosePreference = $VerbosePreference
+$VerbosePreference = 'continue'
+$VMTime = Get-Date
+$LogTime = $VMTime.ToUniversalTime()
+mkdir "C:\Windows\temp\NerdioManagerLogs\ScriptedActions\awsvpnclient" -Force
+Start-Transcript -Path "C:\Windows\temp\NerdioManagerLogs\ScriptedActions\awsvpnclient\ps_log.txt" -Append
+Write-Host "################# New Script Run #################"
+Write-host "Current time (UTC-0): $LogTime"
+
+# Make directory to hold install files
+
+mkdir "C:\Windows\Temp\awsvpnclient\install" -Force
+
+Get-ChildItem C:\Windows\Temp\awsvpnclient\install\ | Remove-Item -Recurse -Force
+
+Invoke-WebRequest -Uri $AwsVpnClientUrl -OutFile "C:\Windows\Temp\awsvpnclient\install\AWS_VPN_Client.msi" -UseBasicParsing
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+cd "C:\Windows\Temp\awsvpnclient\install\"
+
+$Dir = Get-ChildItem C:\Windows\Temp\awsvpnclient\install\ | Where-Object PSIsContainer -eq $true
+if ($Dir.Count -gt 1) {
+    $Dir = Get-ChildItem C:\Windows\Temp\awsvpnclient\ | Where-Object PSIsContainer -eq $true
+}
+
+# Install AWS VPN Client. 
+Write-Host "INFO: Installing AWS VPN Client. . ."
+Start-Process "$($Dir.FullName)\AWS_VPN_Client.msi" `
+    -ArgumentList "ALLUSERS=1 /qn /norestart" `
+    -Wait `
+    -Passthru `
+  
+
+
+Write-Host "INFO: AWS VPN Client install finished."
+
+# End Logging
+Stop-Transcript
+$VerbosePreference=$SaveVerbosePreference
